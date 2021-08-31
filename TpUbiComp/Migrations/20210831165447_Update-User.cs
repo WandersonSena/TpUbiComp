348f6a6d8078e89349a276132d8d4f764bef393d
Wanderson Sena <wanderson.sena@treinus.com.br>
2021-08-31 14:01:59 -0300

    Ajuste na navbar do private

---
+++ TpUbiComp/Migrations/20210831165447_Update-User.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TpUbiComp.Migrations
+{
+    public partial class UpdateUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Birthdate",
+                table: "User",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Cellphone",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Credit",
+                table: "User",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Birthdate",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Cellphone",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Credit",
+                table: "User");
+        }
+    }
+}
--- TpUbiComp/Models/DBS Models/UserModel.cs
+++ TpUbiComp/Models/DBS Models/UserModel.cs
@@ -14,5 +14,11 @@ namespace TpUbiComp.Models
         public string LastName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public string Cellphone { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public double Credit { get; set; }
     }
 }
